d9614e7d00892b65245c58e4ca8a51f8c13919d0
Muqeet-Ryan <muqeetryan@outlook.com>
2026-03-03 19:17:15 +0600

    ""

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/auth.routes.js";
+import productRoutes from "./routes/products.routes.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser"
+
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 const startServer = async () => {
   try {

423a5081ec7ba09e3eb77fa80a6fbe7e9061333b
Muqeet-Ryan <muqeetryan@outlook.com>
2026-03-03 19:15:36 +0600

    models implemented

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -37,20 +37,17 @@ const userSchema = new mongoose.Schema(
       default: "customer",
     },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 // Pre-save hook
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
 
-  try {
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+ 
 });
 
 // Compare password method
